b8fccb5e6ed3c288d884bbfa5c1b89f561e6d0e6
keleris32 <charles.ejiegbu@gmail.com>
2022-11-15 02:40:42 +0100

    feat: added navItem list

---
+++ src/config/navigation.ts
@@ -0,0 +1,12 @@
+export const navigationItems = {
+  sidebar: [
+    {
+      name: 'Overview',
+      to: '/dashboard/overview',
+    },
+    {
+      name: 'Client',
+      to: '/dashboard/client',
+    },
+  ],
+};
